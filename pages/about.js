25f9293d4315ecbef3e128073e7be8f18ecb2938
Ahnzi <jhhh0627@gmail.com>
2022-12-12 14:55:17 +0900

    #1.3 Routing
    #1.3 Routing

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,11 @@
+import NavBar from "../components/Navbar";
+
 // 유저에게 보여주고 싶으면 반드시 export default function()을 해야합니다.
 export default function About() {
-    return "About";
+    return (
+        <div>
+            <NavBar />
+            <h1>About</h1>
+        </div>
+    );
 }
